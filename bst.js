4ada88cc6e682d0f475c68b4e0f309cde54360de
Sam Ringleman <sam.rinleman@gmail.com>
2017-03-26 10:57:20 -0600

    add the binary search tree source code

---
+++ bst.js
@@ -0,0 +1,89 @@
+function BST(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BST.prototype.insert = function(value){
+  if(value <= this.value){
+    if(!this.left){
+      this.left = new BST(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if(!this.right){
+      this.right = new BST(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BST.prototype.contains = function(value){
+  if(value === this.value){
+    return true;
+  } else if(value < this.value){
+    if(!this.left){
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  } else {
+    if(value > this.value){
+      if(!this.right){
+        return false;
+      } else {
+        return this.right.contains(value);
+      }
+    }
+  }
+};
+
+BST.prototype.depthFirstTraversal = function(iteratorFunc, order){
+  if(order === 'pre-order'){
+    iteratorFunc(this.value);
+  }
+  if(this.left){
+    this.left.depthFirstTraversal(iteratorFunc, order);
+  }
+  if(order === 'in-order'){
+    iteratorFunc(this.value);
+  }
+  if(this.right){
+    this.right.depthFirstTraversal(iteratorFunc, order);
+  }
+  if(order === 'post-order'){
+    iteratorFunc(this.value);
+  }
+};
+
+BST.prototype.breadthFirstTraversal = function(iteratorFunc){
+  var queue = [this];
+  while(queue.length){
+    var treenode = queue.shift();
+    iteratorFunc(treenode.value);
+    if(treenode.left){
+      queue.push(treenode.left);
+    }
+    if(treenode.right){
+      queue.push(treenode.right);
+    }
+  }
+};
+
+BST.prototype.getMinVal = function(){
+  if(this.left){
+   return this.left.getMinVal();
+  } else {
+    return this.value;
+  }
+};
+
+BST.prototype.getMaxVal = function(){
+  if(this.right){
+    return this.right.getMaxVal();
+  } else {
+    return this.value;
+  }
+};
